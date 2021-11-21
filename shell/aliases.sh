9742735121d3ff53e1a5ca447555a0aab1d87108
yancy <yyancy517@gmail.com>
2021-11-21 23:12:26 +0800

    changes

--- shell/aliases.sh
+++ shell/aliases.sh
@@ -14,5 +14,5 @@ alias cls='clear'
 alias pp='ps -ef | grep '
 alias languagetool='java -jar /home/yancy/tmp/languagetool/languagetool-standalone/target/LanguageTool-5.6-SNAPSHOT/LanguageTool-5.6-SNAPSHOT/languagetool-commandline.jar'
 alias modx='chmod +x '
-
+alias ssudo='sudo -E env "PATH=$PATH"'
 [ -x "$(command -v nvim)" ] && alias vim="nvim" vi="nvim" nv="nvim" vimdiff="nvim -d"
