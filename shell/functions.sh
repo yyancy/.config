a5578dd1eecc4e65886825dcc32cbc1ae7a4e14c
yyancy <35031396+yyancy@users.noreply.github.com>
2021-12-10 13:08:25 +0800

    Update functions.sh

--- shell/functions.sh
+++ shell/functions.sh
@@ -1,4 +1,31 @@
+# Functions to help us manage paths.  Second argument is the name of the
+# path variable to be modified (default: PATH)
+function pathremove () {
+        local IFS=':'
+        local NEWPATH
+        local DIR
+        local PATHVARIABLE=${2:-PATH}
+        for DIR in ${!PATHVARIABLE} ; do
+                if [ "$DIR" != "$1" ] ; then
+                  NEWPATH=${NEWPATH:+$NEWPATH:}$DIR
+                fi
+        done
+        export $PATHVARIABLE="$NEWPATH"
+}
 
+function pathprepend () {
+        pathremove $1 $2
+        local PATHVARIABLE=${2:-PATH}
+        export $PATHVARIABLE="$1${!PATHVARIABLE:+:${!PATHVARIABLE}}"
+}
+
+function pathappend () {
+        pathremove $1 $2
+        local PATHVARIABLE=${2:-PATH}
+        export $PATHVARIABLE="${!PATHVARIABLE:+${!PATHVARIABLE}:}$1"
+}
+
+export -f pathremove pathprepend pathappend
 
 function mydiff() {
   diff -u $@ | diff-so-fancy
