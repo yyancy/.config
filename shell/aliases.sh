def8909f20f27f311d9fb7af718f2ce4ab3fdb95
yancy <yyancy517@gmail.com>
2022-04-11 22:31:55 +0800

    add new aliases and changes some config

--- shell/aliases.sh
+++ shell/aliases.sh
@@ -37,6 +37,7 @@ alias g28='iconv -f gbk -t utf-8'
 alias fd='fdfind'
 alias less='less --incsearch --ignore-case'
 alias man='man -P "less --incsearch --ignore-case"'
+alias mux='tmuxinator'
 
 [ -x "$(command -v nvim)" ] && alias vim="nvim" vi="nvim" nv="nvim" vimdiff="nvim -d"
 [ -x "$(command -v trash-put)" ] && alias trp="trash-put"
