ec7f0131a74ed8d27183d9cf513ad83d71592464
yancy <35031396+yyancy@users.noreply.github.com>
2023-03-07 18:04:04 +0800

    Update init_basic_dev_env.sh

--- init_basic_dev_env.sh
+++ init_basic_dev_env.sh
@@ -3,3 +3,5 @@
 # 设置vim 和 tmux
 # https://gitee.com/yyancyer/nvim/raw/lua/vimrc.local
 # https://gitee.com/yyancyer/config/raw/master/tmux/tmux.template
+# git clone https://gitee.com/yyancyer/tpm.git ~/.tmux/plugins/tpm
+# for VIM8, git clone https://gitee.com/yyancyer/vim-tmux-navigator.git ~/.vim/pack/plugins/start/vim-tmux-navigator
