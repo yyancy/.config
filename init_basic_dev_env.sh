f051f5b788ca16269c1144971fa67dd45e50e117
yancy <35031396+yyancy@users.noreply.github.com>
2023-05-12 14:15:12 +0800

    Update init_basic_dev_env.sh

--- init_basic_dev_env.sh
+++ init_basic_dev_env.sh
@@ -25,12 +25,30 @@ curl ${vim_template} -o "${vim_conf_path}/vimrc"
 git clone https://gitee.com/yyancyer/vim-tmux-navigator.git \
   ${vim_conf_path}/pack/plugins/start/vim-tmux-navigator
 
+grep -q -i 'centos' /etc/os-release
+is_centos=$?
 
 # install tmux configuration
-if ! which tmux ; then
+if [ $is_centos -ne 0 ] && ! which tmux ; then
   echo "ERROR: cannot find tmux. please install first." >&2
   exit 1
 fi
+
+if [ $is_centos -eq 0 ] ; then
+  echo "INFO: use tmux-3.0a AppImage..."
+  app_name=tmux-3.0a.AppImage
+  curl -fsSL https://gitee.com/yyancyer/config/raw/master/tmux-3.0a-x86_64.AppImage -o /tmp/$app_name
+  sudo mv -vf /tmp/$app_name /usr/local/bin
+  sudo chmod +x /usr/local/bin/$app_name
+  if command -v /usr/local/bin/tmux &> /dev/null ; then
+     echo "backup previous tmux"
+     sudo mv -vf /usr/local/bin/tmux /usr/local/bin/tmux.old
+  fi
+  ln -sv /usr/local/bin/$app_name /usr/local/bin/tmux
+fi
+
+
+
 
 if [[ ! -d ${tmux_conf_path} ]]; then
   mkdir -v "${tmux_conf_path}"
