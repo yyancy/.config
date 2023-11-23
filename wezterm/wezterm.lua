f700363f989fc044ddd75194677805a62ae1d8a7
yancy <yyancy517@gmail.com>
2023-11-23 22:57:25 +0800

    feat: add configuration for wezterm

---
+++ wezterm/wezterm.lua
@@ -0,0 +1,86 @@
+-- Pull in the wezterm API
+local wezterm = require("wezterm")
+local act = wezterm.action
+
+-- This table will hold the configuration.
+local config = {}
+-- config.use_ime = true
+-- In newer versions of wezterm, use the config_builder which will
+-- help provide clearer error messages
+if wezterm.config_builder then
+	config = wezterm.config_builder()
+end
+
+-- This is where you actually apply your config choices
+
+-- For example, changing the color scheme:
+-- config.color_scheme = "AdventureTime"
+config.window_background_opacity = 0.8
+config.harfbuzz_features = { "liga=1" }
+config.font = wezterm.font({ family = "Fira Code", harfbuzz_features = { "calt=1", "clig=1", "liga=1" } })
+config.font_size = 14.0
+
+-- Show which key table is active in the status area
+wezterm.on("update-right-status", function(window, pane)
+	local name = window:active_key_table()
+	if name then
+		name = "TABLE: " .. name
+	end
+	window:set_right_status(name or "")
+end)
+
+-- timeout_milliseconds defaults to 1000 and can be omitted
+config.leader = { key = "z", mods = "ALT", timeout_milliseconds = 1000 }
+config.keys = {
+	{
+		key = "|",
+		mods = "LEADER|SHIFT",
+		action = wezterm.action.SplitHorizontal({ domain = "CurrentPaneDomain" }),
+	},
+	{
+		key = "-",
+		mods = "LEADER",
+		action = wezterm.action.SplitVertical({ domain = "CurrentPaneDomain" }),
+	},
+	-- CTRL-a, followed by CTRL-o will switch back to the last active tab
+	{
+		key = "o",
+		mods = "LEADER|CTRL",
+		action = wezterm.action.ActivateLastTab,
+	},
+	{
+		key = "!",
+		mods = "LEADER | SHIFT",
+		action = wezterm.action_callback(function(win, pane)
+			local tab, window = pane:move_to_new_tab()
+		end),
+	},
+	-- Send "CTRL-A" to the terminal when pressing CTRL-A, CTRL-A
+	{
+		key = "a",
+		mods = "LEADER|CTRL",
+		action = wezterm.action.SendKey({ key = "a", mods = "CTRL" }),
+	},
+	{ key = "LeftArrow", mods = "LEADER", action = act.ActivatePaneDirection("Left") },
+	{ key = "h", mods = "LEADER", action = act.ActivatePaneDirection("Left") },
+
+	{ key = "RightArrow", mods = "LEADER", action = act.ActivatePaneDirection("Right") },
+	{ key = "l", mods = "LEADER", action = act.ActivatePaneDirection("Right") },
+
+	{ key = "UpArrow", mods = "LEADER", action = act.ActivatePaneDirection("Up") },
+	{ key = "k", mods = "LEADER", action = act.ActivatePaneDirection("Up") },
+
+	{ key = "DownArrow", mods = "LEADER", action = act.ActivatePaneDirection("Down") },
+	{ key = "j", mods = "LEADER", action = act.ActivatePaneDirection("Down") },
+}
+for i = 1, 8 do
+	-- CMD+ALT + number to activate that window
+	table.insert(config.keys, {
+		key = tostring(i),
+		mods = "LEADER",
+		action = act.ActivateTab(i - 1),
+	})
+end
+
+-- and finally, return the configuration to wezterm
+return config
