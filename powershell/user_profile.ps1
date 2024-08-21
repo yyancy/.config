573df39157ca53aa12aff10b654ae78e3bed6902
dongyang <dongyang@wondertek.com.cn>
2024-08-21 17:18:32 +0800

    add some configurations

--- powershell/user_profile.ps1
+++ powershell/user_profile.ps1
@@ -20,6 +20,15 @@ Import-Module PSFzf
 Set-PsFzfOption -PSReadlineChordProvider 'Ctrl+t' -PSReadlineChordReverseHistory 'Ctrl+r'
 
 
+function yy {
+  $tmp = [System.IO.Path]::GetTempFileName()
+    yazi $args --cwd-file="$tmp"
+    $cwd = Get-Content -Path $tmp
+    if (-not [String]::IsNullOrEmpty($cwd) -and $cwd -ne $PWD.Path) {
+      Set-Location -LiteralPath $cwd
+    }
+  Remove-Item -Path $tmp
+}
 # function for alias
 function get-git { git $args }
 function get-gitstatus { git status }
